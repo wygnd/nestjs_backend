b119fede24b1fa208f9d38040fba36fd4a21288e
Denis <wygndo@gmail.com>
2024-02-07 08:33:59 +0300

    add rest api description and title endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,35 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {SequelizeModule} from "@nestjs/sequelize";
-import { UsersModule } from './users/users.module';
+import {UsersModule} from './users/users.module';
 import {ConfigModule} from "@nestjs/config";
 import {UserModel} from "./users/user.model";
-import { RolesModule } from './roles/roles.module';
+import {RolesModule} from './roles/roles.module';
 import {RolesModel} from "./roles/roles.model";
 import {UserRolesModel} from "./roles/user-roles.model";
-import { AuthModule } from './auth/auth.module';
+import {AuthModule} from './auth/auth.module';
 
 @Module({
-  imports: [
-      ConfigModule.forRoot({
-          envFilePath: `.${process.env.NODE_ENV}.env`
-      }),
-      SequelizeModule.forRoot({
-        dialect: "mysql",
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        models: [UserModel, RolesModel, UserRolesModel],
-        autoLoadModels: true,
-      }),
-      UsersModule,
-      RolesModule,
-      AuthModule
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        SequelizeModule.forRoot({
+            dialect: "mysql",
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            models: [UserModel, RolesModel, UserRolesModel],
+            autoLoadModels: true,
+            synchronize: true,
+        }),
+        UsersModule,
+        RolesModule,
+        AuthModule
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {AuthService} from './auth.service';
-import {ApiTags} from "@nestjs/swagger";
+import {ApiOperation, ApiProperty, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {CreateUserDto} from "../users/dto/create-user.dto";
+import {UserModel} from "../users/user.model";
 
 @ApiTags('Authorization')
 @Controller('auth')
@@ -9,12 +10,16 @@ export class AuthController {
     constructor(private readonly authService: AuthService) {
     }
 
-    @Post('/login')
+    @ApiOperation({summary: "Авторизация пользователя"})
+    @ApiResponse({status: 200, type: UserModel})
+    @Post('/singin')
     async login(@Body() user: CreateUserDto) {
       return this.authService.login(user);
     }
 
-    @Post('/registration')
+    @ApiOperation({summary: "Регистрация пользователя"})
+    @ApiResponse({status: 200, type: UserModel})
+    @Post('/signup')
     async registration(@Body() user: CreateUserDto) {
       return this.authService.registration(user);
     }
